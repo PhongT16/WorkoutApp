f11325bfe6f89d5ba86115a1e4048eded27dda59
PhongT16 <36669438+PhongT16@users.noreply.github.com>
2019-12-03 15:57:44 -0600

    Basic idea of app is completed.

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,72 +13,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Vibrator chestVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+
         Button chest = findViewById(R.id.chest);
         chest.setOnClickListener(unused -> {
             // Change the label's text
+            chestVibrator.vibrate(500);
             Intent intent = new Intent(this, chest.class);
             startActivity(intent);
         });
+        Vibrator leftShoulderVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
         Button leftShoulder = findViewById(R.id.leftShoulder);
         leftShoulder.setOnClickListener(unused -> {
             // Change the label's text
+            leftShoulderVibrator.vibrate(500);
             Intent intent = new Intent(this, shoulders.class);
             startActivity(intent);
         });
-
+        Vibrator rightShoulderVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button rightShoulder = findViewById(R.id.rightShoulder);
         rightShoulder.setOnClickListener(unused -> {
             // Change the label's text
+            rightShoulderVibrator.vibrate(500);
             Intent intent = new Intent(this, shoulders.class);
             startActivity(intent);
         });
-
+        Vibrator leftBicepVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button leftBicep = findViewById(R.id.leftBicep);
         leftBicep.setOnClickListener(unused -> {
             // Change the label's text
+            leftBicepVibrator.vibrate(500);
             Intent intent = new Intent(this, Biceps.class);
             startActivity(intent);
         });
-
+        Vibrator rightBicepVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button rightBicep = findViewById(R.id.rightBicep);
         rightBicep.setOnClickListener(unused -> {
             // Change the label's text
+            rightBicepVibrator.vibrate(500);
             Intent intent = new Intent(this, Biceps.class);
             startActivity(intent);
         });
-
+        Vibrator leftForearmVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button leftForearm = findViewById(R.id.leftForearm);
         leftForearm.setOnClickListener(unused -> {
             // Change the label's text
+            leftForearmVibrator.vibrate(500);
             Intent intent = new Intent(this, Forearm.class);
             startActivity(intent);
         });
-
+        Vibrator rightForearmVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button rightForearm = findViewById(R.id.rightForearm);
         rightForearm.setOnClickListener(unused -> {
             // Change the label's text
+            rightForearmVibrator.vibrate(500);
             Intent intent = new Intent(this, Forearm.class);
             startActivity(intent);
         });
-
+        Vibrator absVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         Button abs = findViewById(R.id.abs);
         abs.setOnClickListener(unused -> {
             // Change the label's text
+            absVibrator.vibrate(500);
             Intent intent = new Intent(this, Abs.class);
             startActivity(intent);
         });
-
-        Button leftQuads = findViewById(R.id.leftQuad);
-        leftQuads.setOnClickListener(unused -> {
+        Vibrator leftQuadVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        Button leftQuad = findViewById(R.id.leftQuad);
+        leftQuad.setOnClickListener(unused -> {
             // Change the label's text
+            leftQuadVibrator.vibrate(500);
             Intent intent = new Intent(this, Quads.class);
             startActivity(intent);
         });
-
-        Button rightQuads = findViewById(R.id.rightQuad);
-        rightQuads.setOnClickListener(unused -> {
+        Vibrator rightQuadVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        Button rightQuad = findViewById(R.id.rightQuad);
+        rightQuad.setOnClickListener(unused -> {
             // Change the label's text
+            rightQuadVibrator.vibrate(500);
             Intent intent = new Intent(this, Quads.class);
             startActivity(intent);
         });
